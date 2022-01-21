1dafb97b01b2f7670e01aa67497b87653ca6de1f
R.P. Muhammad Bahtiar Subrata <74234312+tiarss@users.noreply.github.com>
2022-01-21 18:39:34 +0700

    Revert "Cards home (#1)"
    This reverts commit e1972fcd9c39c178b94ddeb48a5c68dc6880fd8e.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
-import Header from './components/Header';
-import "./App.css"
-import LoginPage from './pages/LoginPage';
-import CardsHome from './components/CardsHome';
-// {/* <LoginPage /> */}
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <Header />
-      <CardsHome /> */}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
