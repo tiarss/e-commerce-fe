8c0b7761651ed314453ffbdd82adc1d8d4ad434c
R.P. Muhammad Bahtiar Subrata <subratabahtiar@outlook.com>
2022-01-21 15:33:07 +0700

    cards responsif

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './components/Header';
 import "./App.css"
 import LoginPage from './pages/LoginPage';
+import CardsHome from './components/CardsHome';
+// {/* <LoginPage /> */}
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* <LoginPage /> */}
+      <CardsHome />
     </div>
   );
 }
