fbe813a66b88057ade71da5c47203e75a9b21653
R.P. Muhammad Bahtiar Subrata <subratabahtiar@outlook.com>
2022-01-21 18:27:39 +0700

    delete comment in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,12 @@ import Header from './components/Header';
 import "./App.css"
 import LoginPage from './pages/LoginPage';
 import CardsHome from './components/CardsHome';
-// {/* <LoginPage /> */}
+
 
 function App() {
   return (
     <div className="App">
-      {/* <Header />
-      <CardsHome /> */}
+
     </div>
   );
 }
