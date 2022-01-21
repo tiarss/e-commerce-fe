e1d9be1907294599ccfc9a6a0cf1a486a97f9327
R.P. Muhammad Bahtiar Subrata <subratabahtiar@outlook.com>
2022-01-21 09:41:53 +0700

    add mui and header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Box } from "@mui/material";
+import logo from "../images/Logo-sirclo.png"
+
+function Header() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        height: "70px",
+        padding: "10px 50px",
+        boxShadow: "0px 4px 4px #000",
+        justifyContent: "space-between",
+        alignItems: "center"
+      }}>
+      <Box>
+        <img style={{height: "40px"}} src={logo} alt='logo-sirclo' />
+      </Box>
+      <Box>
+        <Box>Search</Box>
+      </Box>
+      <Box>Login and Cart</Box>
+    </Box>
+  );
+}
+
+export default Header;
