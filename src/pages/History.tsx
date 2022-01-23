64058a8d3af8f23c01feb65ace781d303d8ae562
R.P. Muhammad Bahtiar Subrata <subratabahtiar@outlook.com>
2022-01-23 07:13:17 +0700

    history first

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import { Box, Typography } from "@mui/material";
 function History() {
   return (
-    <Box>
+    <Box
+      sx={{
+        minHeight: "400px",
+        boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.250);",
+        borderRadius: "10px",
+        marginTop: "10px",
+        marginBottom: "50px",
+        padding: "20px",
+        display: "flex",
+      }}>
       <Typography>History</Typography>
     </Box>
   );
